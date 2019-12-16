83957c29a42aafccb94092f175e9fbcf2b6e53c1
tillklockmann <till.klockmann@gmail.com>
2019-12-16 22:03:32 +0100

    basic overhaul, readme, svg img

---
+++ README.md
@@ -0,0 +1,2 @@
+# KV-KV nonesense word generator
+See the demo [here](http://kvkv.neu-start-st-pauli.de/)
--- index.php
+++ index.php
@@ -12,37 +12,24 @@ $maxv =  count($vok) - 1;
      <meta charset="utf-8">
      <title></title>
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-      <link href="https://fonts.googleapis.com/css?family=Amaranth|Be+Vietnam|Inder|Livvic|Montserrat+Alternates&display=swap" rel="stylesheet">
-      <style media="screen">
-        td {
-          font-family: 'Montserrat Alternates', sans-serif;
-          font-family: 'Amaranth', sans-serif;
-          font-family: 'Be Vietnam', sans-serif;
-          font-family: 'Livvic', sans-serif;
-          /* font-family: 'Inder', sans-serif; */
-        }
-        td span {
-          font-size: 320px;
-        }
-      </style>
+      <link href="https://fonts.googleapis.com/css?family=Livvic&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="/style.css">
    </head>
    <body>
      <div class="w3-row">
-         <table class="w3-table w3-bordered w3-border" >
+         <table class="w3-table" >
            <tr>
              <td class="w3-border-right w3-center w3-padding-64 w3-jumbo">
-               <span><?= $kon[rand(0,$maxk)]  ?></span>
-               <span class="w3-text-green"><?= $vok[rand(0, $maxv)] ?></span>
-               <span class="w3-margin-left w3-margin-right">-</span>
-               <span><?=  $kon[rand(0,$maxk)] ?></span>
-               <span class="w3-text-green"><?= $vok[rand(0, $maxv)] ?></span>
+               <span><?= $kon[rand(0,$maxk)]  ?></span><span class="w3-text-green"><?= $vok[rand(0, $maxv)] ?></span><span class="w3-margin-left w3-margin-right">-</span><span><?=  $kon[rand(0,$maxk)] ?></span><span class="w3-text-green"><?= $vok[rand(0, $maxv)] ?></span>
              </td>
            </tr>
          </table>
        </div>
        <div class="w3-row">
-         <div class="w3-margin-top w3-container">
-
+         <div class="w3-margin-bottom w3-container w3-center" >
+           <button class="w3-btn w3-round-xxlarge w3-padding-32 w3-light-grey w3-xlarge" onclick="location.reload();">
+             <img src="refresh.svg" alt="Neues Wort">
+            </button>
          </div>
        </div>
 
